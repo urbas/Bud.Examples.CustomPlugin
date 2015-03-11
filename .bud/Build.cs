3c9bc78ec664d9fa9c50e3a9868e5f3b5d728ff1
Matej Urbas <matej.urbas@gmail.com>
2015-03-11 12:04:43 +0000

    Formatted the code a bit. Gitignored more folders.

--- .bud/Build.cs
+++ .bud/Build.cs
@@ -1,13 +1,10 @@
 using Bud;
 using Bud.CSharp;
 using Bud.Projects;
-using System.IO;
-using System;
 
 public class Build : IBuild {
   public Settings Setup(Settings settings, string baseDir) {
-    return settings
-      .Version("1.2.3")
-      .Project("Bud.Examples.SetVersion", baseDir, Cs.Exe());
+    return settings.Version("1.2.3")
+                   .Project("Bud.Examples.SetVersion", baseDir, Cs.Exe());
   }
 }
